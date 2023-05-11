1b9158b0f732f3ede86e59bc1dc5d093c2e3cee7
Jaehyeon Nam <njh961@naver.com>
2023-05-11 12:37:08 +0900

    몬스터 기능 추가

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public Transform[] spawnPoint;
+    public SpawnData[] spawnData;
+    int level;
     float timer;
-    // Start is called before the first frame update
+    
 
     void Awake()
     {
@@ -15,8 +17,9 @@ public class Spawner : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
+        level = Mathf.Min(Mathf.FloorToInt(GameManager.instance.gameTime / 10f), spawnData.Length - 1);
 
-        if(timer > 0.2f)
+        if(timer > spawnData[level].spawnTime)
         {
            
             timer = 0;
@@ -27,8 +30,18 @@ public class Spawner : MonoBehaviour
     //소환 함수
     void Spawn()
     {
-        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0,2));
+        GameObject enemy = GameManager.instance.pool.Get(0);
         enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
+        enemy.GetComponent<Enemy>().Init(spawnData[level]);
     }
     
+}
+
+[System.Serializable]
+public class SpawnData
+{
+    public int spriteType;
+    public float spawnTime;
+    public int health;
+    public float speed;
 }
